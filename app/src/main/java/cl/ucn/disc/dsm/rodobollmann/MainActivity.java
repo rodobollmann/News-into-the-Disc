9e31050a4d1c282771fd46c310c00ceed2a3e485
rodobollmann <rodolfo.bollmann1@gmail.com>
2021-06-14 19:07:55 -0400

    style(Theme): Add colors, change the prefix, add the toolbar

--- app/src/main/java/cl/ucn/disc/dsm/rodobollmann/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/rodobollmann/MainActivity.java
@@ -1,14 +1,53 @@
+/*
+ * Copyright (c) 2021. Rodolfo Bollmann Checura
+ */
+
 package cl.ucn.disc.dsm.rodobollmann;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MainActivity extends AppCompatActivity {
+import cl.ucn.disc.dsm.rodobollmann.databinding.ActivityMainBinding;
 
+/**
+ *  The Main class.
+ *
+ * @author Rodolfo Bollmann Checura.
+ */
+public final class MainActivity extends AppCompatActivity {
+
+    /**
+     *  The Logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+    /**
+     * The Binding
+     */
+    private ActivityMainBinding binding;
+
+    /**
+     * @param savedInstanceState the instance
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Inflate the xml
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
+
+        // Set the toolbar
+        this.setSupportActionBar(this.binding.toolbar);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log.debug("onStart ..");
+    }
+
 }
